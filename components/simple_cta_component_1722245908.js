d4b6493ce45e86f7d845fbb6b49e7b808dc75fba
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-29 09:39:29 +0000

    Code generation

--- components/simple_cta_component_1722245908.js
+++ components/simple_cta_component_1722245908.js
@@ -16,6 +16,7 @@ Vue.component("simple_cta_component_1722245908", {
                 <div id="cta-button-container" class="flex space-x-4">
                     <a id="cta-button" href="#" class="flex-1 text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Start Building Now</a>
                     <a id="cta-button-2" href="#" class="flex-1 text-purple-700 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 border border-purple-700 focus:outline-none">Learn More</a>
+                    <a id="cta-button-3" href="#" class="flex-1 text-white bg-black hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-700">Contact Us</a>
                 </div>
             </div>
         </div>
